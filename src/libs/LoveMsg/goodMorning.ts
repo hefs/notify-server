e130e01785960967fa58f4cfe98a6783f060e721
hefasong <hefs@jxjee.com>
2022-04-29 10:04:01 +0800

    12

--- src/libs/LoveMsg/goodMorning.ts
+++ src/libs/LoveMsg/goodMorning.ts
@@ -47,7 +47,7 @@ const goodWord = async () => {
 
 // 天气信息
 const weatherInfo = async () => {
-  const weather = await API.getWeather('南昌')
+  const weather = await API.getWeather('抚州市金溪县')
   if (weather) {
     const lunarInfo = await API.getLunarDate(weather.date)
     const template = textCardTemplate({ ...weather, lunarInfo })
